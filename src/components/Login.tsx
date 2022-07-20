bd19e60287cefd21619b4bb2c8f6665c2010fcf4
Mustapha Mubarak <58697223+mako-commits@users.noreply.github.com>
2022-07-20 14:54:42 +0100

    ✨ feat: Implement login form validation

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,35 +1,45 @@
 import { useState } from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Alert } from "react-bootstrap";
 import axios from "axios";
+import useInput from "./hooks/use-input";
 
+//check if input field is empty
+const isNotEmpty = (value: string) => value.trim() !== "";
+
+//check length of password
+const lenPassword = (value: string) => value.length >= 6;
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [usernameFieldTouched, setUsernameFieldTouched] = useState(false);
-
-  const [password, setPassword] = useState("");
-  const [passwordFieldTouched, setPasswordFieldTouched] = useState(false);
   const [login, setLogin] = useState(false);
+  const [failedLogin, setFailedLogin] = useState(false);
+  const {
+    value: username,
+    isValid: usernameIsValid,
+    hasError: usernameHasError,
+    valueChangeHandler: usernameChangeHandler,
+    inputBlurHandler: usernameBlurHandler,
+    reset: resetUsername,
+  } = useInput(isNotEmpty);
 
-  const enterdUsernameIsValid = username.trim() !== "";
-  const usernameInputIsInvalid = !enterdUsernameIsValid && usernameFieldTouched;
-
-  const enterdPasswordIsValid = password.trim() !== "";
-  const passwordInputIsInvalid = !enterdPasswordIsValid && passwordFieldTouched;
+  const {
+    value: password,
+    isValid: passwordIsValid,
+    valueChangeHandler: passwordChangeHandler,
+    hasError: passwordHasError,
+    inputBlurHandler: passwordBlurHandler,
+    reset: resetPassword,
+  } = useInput(lenPassword);
 
   let formIsValid = false;
-  const handleUsernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setUsername(e.target.value);
-  };
-  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-  };
+
+  if (usernameIsValid && passwordIsValid) {
+    formIsValid = true;
+  }
+
   const handleFormSubmit = (
     e: React.FormEvent<HTMLFormElement> | React.MouseEvent<HTMLButtonElement>
   ) => {
     // prevent the form from refreshing the whole page
     e.preventDefault();
-    // make a popup alert showing the "submitted" text
-    alert("Submited");
 
     //configuration
     const config = {
@@ -44,50 +54,94 @@ const Login = () => {
     axios(config)
       .then((result) => {
         setLogin(true);
+        // make a popup alert showing the "submitted" text
+        // alert("Login Successful");
       })
       .catch((error) => {
         error = new Error("Unable to login");
+        setFailedLogin(true);
+        // alert("Login failed");
       });
+
+    resetUsername();
+    resetPassword();
   };
+
+  //set username class based on validity
+  const usernameClasses = usernameHasError
+    ? "form-control invalid"
+    : "form-control";
+
+  //set password class based on validity
+  const passwordClasses = passwordHasError
+    ? "form-control invalid"
+    : "form-control";
+
   return (
     <>
       <h2>Login</h2>
       <Form onSubmit={handleFormSubmit}>
         {/* Username Field*/}
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Group className={usernameClasses} controlId="formBasicEmail">
           <Form.Label>Username</Form.Label>
           <Form.Control
             type="text"
             name="username"
             value={username}
-            onChange={handleUsernameChange}
+            onChange={usernameChangeHandler}
+            onBlur={usernameBlurHandler}
             placeholder="Enter a username"
           />
+          {usernameHasError && (
+            <p className="error-text">Please enter a a username.</p>
+          )}
         </Form.Group>
 
         {/* Password Field*/}
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Group className={passwordClasses} controlId="formBasicEmail">
           <Form.Label>Password</Form.Label>
           <Form.Control
             type="password"
             name="password"
             value={password}
-            onChange={handlePasswordChange}
+            onChange={passwordChangeHandler}
+            onBlur={passwordBlurHandler}
             placeholder="Enter a password"
           />
+          {passwordHasError && (
+            <p className="error-text">Password must be at least 6 characters</p>
+          )}
         </Form.Group>
         {/* Submit Button */}
-        <Button variant="primary" type="submit" onClick={handleFormSubmit}>
+        <Button
+          variant="primary"
+          type="submit"
+          onClick={handleFormSubmit}
+          disabled={!formIsValid}
+          className="mb-3"
+        >
           Login
         </Button>
 
         {/* display staus message */}
-        {login ? (
-          <p className="text-success">Login was successful</p>
-        ) : (
-          <p className="text-danger">
-            Something went wrong with while logining
-          </p>
+        {login && (
+          <Alert variant="success" onClose={() => setLogin(false)} dismissible>
+            <Alert.Heading>Login Successful</Alert.Heading>
+            <p>Proceed to login</p>
+          </Alert>
+        )}
+        {failedLogin && (
+          <Alert
+            variant="danger"
+            onClose={() => setFailedLogin(false)}
+            dismissible
+          >
+            <Alert.Heading>Login Failed</Alert.Heading>
+            <p>
+              Make sure you register before you login or cross-check your
+              credetials
+            </p>
+          </Alert>
         )}
       </Form>
     </>
--- src/components/Register.tsx
+++ src/components/Register.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Alert } from "react-bootstrap";
 import useInput from "../components/hooks/use-input";
-type Props = {};
+
 //check if input field is empty
 const isNotEmpty = (value: string) => value.trim() !== "";
 //validate email
@@ -10,7 +10,7 @@ const isEmail = (value: string | string[]) => value.includes("@");
 //check length of password
 const lenPassword = (value: string) => value.length >= 6;
 
-const Register: React.FC<Props> = () => {
+const Register = () => {
   const [register, setRegister] = useState(false);
   const [failedRegister, setFailedRegister] = useState(false);
 
@@ -72,16 +72,20 @@ const Register: React.FC<Props> = () => {
     axios(config)
       .then((result) => {
         setRegister(true);
+        // make a popup alert showing the "submitted" text
+        // alert("Registration Complete");
       })
       .catch((error) => {
         error = new Error("Unable to register user");
         setFailedRegister(true);
+        // make a popup alert showing the "submitted" text
+        // alert("Registration failed");
       });
     // console.log(username);
     // console.log(email);
     // console.log(password);
     // make a popup alert showing the "submitted" text
-    alert("Submited");
+    // alert("Submited");
     resetUsername();
     resetPassword();
     resetEmail();
@@ -159,16 +163,32 @@ const Register: React.FC<Props> = () => {
           type="submit"
           onClick={handleFormSubmit}
           disabled={!formIsValid}
+          className="mb-3"
         >
           Register
         </Button>
 
         {/* display status message */}
         {register && (
-          <p className="text-success">You have been regsitered successfully</p>
+          <Alert
+            variant="success"
+            onClose={() => setRegister(false)}
+            dismissible
+          >
+            <Alert.Heading>You have been regsitered successfully</Alert.Heading>
+            <p>Proceed to login</p>
+          </Alert>
+          // <p className="text-success">You have been regsitered successfully</p>
         )}
         {failedRegister && (
-          <p className="text-danger">Unable to register user</p>
+          <Alert
+            variant="danger"
+            onClose={() => setFailedRegister(false)}
+            dismissible
+          >
+            <Alert.Heading>Unable to register user</Alert.Heading>
+            <p>Cross-check your credentials</p>
+          </Alert>
         )}
       </Form>
     </>
