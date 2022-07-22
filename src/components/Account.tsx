6e1600d7147ca62f6db43d5649b72029e50a70b8
Mustapha Mubarak <58697223+mako-commits@users.noreply.github.com>
2022-07-22 21:57:02 +0100

    feat: Add switch tab for login and register

--- src/components/Account.tsx
+++ src/components/Account.tsx
@@ -1,19 +1,27 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Col } from "react-bootstrap";
 import Login from "./Login";
 import Register from "./Register";
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
 
 const Account = () => {
   return (
     <Container>
-      <Row>
-        <Col xs={12} sm={12} md={6} lg={6}>
-          <Register />
-        </Col>
-        <Col xs={12} sm={12} md={6} lg={6}>
-          <Login />
-        </Col>
-      </Row>
+      <Col xs={12} sm={12} md={6} lg={6} className="mx-auto">
+        <Tabs
+          defaultActiveKey="register"
+          transition={false}
+          id="fill-tab-example"
+          className="mb-3 justify-content-center"
+        >
+          <Tab eventKey="register" title="Register">
+            <Register />
+          </Tab>
+          <Tab eventKey="login" title="Login">
+            <Login />
+          </Tab>
+        </Tabs>
+      </Col>
     </Container>
   );
 };
