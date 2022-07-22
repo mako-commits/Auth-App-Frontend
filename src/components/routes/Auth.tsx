0de057c72f244089237755f799101e5df530019c
Mustapha Mubarak <58697223+mako-commits@users.noreply.github.com>
2022-07-22 20:54:40 +0100

    feat: Implement logout feature

--- src/components/routes/Auth.tsx
+++ src/components/routes/Auth.tsx
@@ -1,7 +1,51 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Cookies from "universal-cookie";
+import { Button } from "react-bootstrap";
+const cookies = new Cookies();
+
+// get token generated on login
+const token = cookies.get("TOKEN");
 
 const Auth = () => {
-  return <div>Auth</div>;
+  const [message, setMessage] = useState("");
+
+  // useEffect automatically executes once the page is fully loaded
+  useEffect(() => {
+    const configuration = {
+      method: "get",
+      url: "https://auth-app--nodejs.herokuapp.com/auth",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    };
+
+    //make the API call
+    axios(configuration)
+      .then((result) => {
+        //assign the message in our result to the message we initialized
+        setMessage(result.data.message);
+      })
+      .catch((error) => {
+        error = new Error();
+      });
+  }, []);
+
+  //logout functionality
+  const logout = () => {
+    //destroy the cookie
+    cookies.remove("TOKEN", { path: "/  " });
+    // redirect user to the landing page
+    window.location.href = "/";
+  };
+  return (
+    <div className="text-center mb-5">
+      <h3 className="text-center text-danger">{message}</h3>
+      <Button type="submit" variant="danger" onClick={logout}>
+        Logout
+      </Button>
+    </div>
+  );
 };
 
 export default Auth;
--- src/components/routes/Public.tsx
+++ src/components/routes/Public.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 const Public = () => {
-  return <div>Public Component</div>;
+  const [message, setMessage] = useState(" ");
+
+  useEffect(() => {
+    //set configurations for the API call
+    const configuration = {
+      method: "get",
+      url: "https://auth-app--nodejs.herokuapp.com/public",
+    };
+
+    //make the API call
+    axios(configuration)
+      .then((result) => {
+        //assign the message in our result to the message initialized
+        setMessage(result.data.message);
+      })
+      .catch((error) => {
+        error = new Error();
+      });
+  }, []);
+  return (
+    <div>
+      {/* displaying our message from our API call */}
+      <h3 className="text-center text-danger">{message}</h3>
+    </div>
+  );
 };
 
 export default Public;
